849ed1a2c7577a462845d38e40054472d09aa933
Zambros777 <zambros777@gmail.com>
2024-08-15 08:59:21 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -90,7 +90,7 @@ app.post('/api/wallet-balance', (req, res) => {
 
 app.get("/api/view-balances", (req, res) => {
   const walletData = readData();
-  console.log(walletData)
+  
   const tableRows = walletData
     .map(
       (entry) => `
@@ -153,7 +153,7 @@ app.get("/api/view-balances", (req, res) => {
     </html>
   `;
 
-  res.send(html);
+  res.send(walletData);
 });
 
 // Запуск сервера для локального тестирования
